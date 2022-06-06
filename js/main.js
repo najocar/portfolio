854440bd3f0b1cb538bd52ffa90ac1fc15b2d2b6
jose carlos <josecarloscastrolopez30@gmail.com>
2022-06-06 17:27:52 +0200

    1st commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+let animado = document.querySelectorAll(".animado");
+let topnav = document.getElementById("barra");
+let inicio = document.getElementById("inicio");
+let alturaInicio = inicio.offsetHeight;
+
+function mostrarScroll() {
+    let scrollTop = document.documentElement.scrollTop;
+    for (let i = 0; i < animado.length; i++) {
+        let alturaAnimado = animado[i].offsetTop;
+        if (alturaAnimado - 200 < scrollTop) {
+
+            animado[i].classList.add("mostrarDerecha")
+            animado[i].style.opacity = 1;
+        }
+    }
+    if (scrollTop > alturaInicio -50) {
+        console.log("hola");
+        barra.classList.remove("topnavAlta");
+        barra.classList.add("topnavBaja")
+
+    } else {
+
+        barra.classList.add("topnavAlta")
+    }
+
+}
+barra.classList.add("topnavAlta")
+window.addEventListener('scroll', mostrarScroll);
