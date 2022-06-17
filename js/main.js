164d092ad8b378844bd8b7da1007514d4f5dda4e
najocar <josecarloscastrolopez30@gmail.com>
2022-06-17 14:57:46 +0200

    proyectos

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 let animado = document.querySelectorAll(".animado");
 let topnav = document.getElementById("barra");
 let inicio = document.getElementById("inicio");
+let proyectos = document.getElementById("proyectos");
 let alturaInicio = inicio.offsetHeight;
+let alturaProyectos = proyectos.getBoundingClientRect();
 
 function mostrarScroll() {
     let scrollTop = document.documentElement.scrollTop;
     for (let i = 0; i < animado.length; i++) {
         let alturaAnimado = animado[i].offsetTop;
         if (alturaAnimado - 200 < scrollTop) {
-
             animado[i].classList.add("mostrarDerecha")
             animado[i].style.opacity = 1;
         }
@@ -21,6 +22,12 @@ function mostrarScroll() {
 
         barra.classList.add("topnavAlta")
     }
+    // if (scrollTop >= alturaProyectos.y - 200) {
+    //     document.body.style.backgroundColor = 'gray';
+
+    // } else {
+    //     document.body.style.backgroundColor = 'white';
+    // }
 
 }
 barra.classList.add("topnavAlta")
